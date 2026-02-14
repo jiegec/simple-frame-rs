ba0c4138587837e3b3417014e9e44b8cc81bb071
Jiajie Chen <c@jia.je>
2026-02-14 17:37:09 +0800

    Add binutils version to testcase name

--- lib/create_testcase.sh
+++ lib/create_testcase.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 set -x -e
+SUFFIX=$(uname -m)-$(LANG=C as --version | grep -oP '\d+\.\d+' | head -1)
+echo $SUFFIX
 echo "void foo() {}\nvoid bar() {foo();}\nint main() { return 0; }" > test.c
 
 gcc -Wa,--gsframe test.c -o test
 objdump --sframe test
 cargo run --example dump_sframe test
 cargo run --example create_testcase test
-mv testcases/test.json testcases/test-$(uname -m).json
+mv testcases/test.json testcases/test-${SUFFIX}.json
 
 gcc -Wa,--gsframe test.c -o test-fp -fomit-frame-pointer
 objdump --sframe test-fp
 cargo run --example dump_sframe test-fp
 cargo run --example create_testcase test-fp
-mv testcases/test-fp.json testcases/test-fp-$(uname -m).json
+mv testcases/test-fp.json testcases/test-fp-${SUFFIX}.json
 
 rm -rf test.c test test-fp
