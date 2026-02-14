9980c3424479c42888bc8265632b56c80597343a
Jiajie Chen <c@jia.je>
2026-02-14 17:55:17 +0800

    Test more binutils versions

--- lib/create_testcase.sh
+++ lib/create_testcase.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -x -e
-SUFFIX=$(uname -m)-$(LANG=C as --version | grep -oP '\d+\.\d+' | head -1)
+SUFFIX=$(uname -m)-$(LANG=C PATH=$CUSTOM_BINUTILS_PATH:$PATH as --version | grep -oP '\d+\.\d+' | head -1)
 echo $SUFFIX
 echo -e "void foo() {}\nvoid bar() {foo();}\nint main() { return 0; }" > test.c
 
-gcc -Wa,--gsframe test.c -o test
+PATH=$CUSTOM_BINUTILS_PATH:$PATH gcc -Wa,--gsframe test.c -o test
 objdump --sframe test
 cargo run --example dump_sframe test
 cargo run --example create_testcase test
 mv testcases/test.json testcases/test-${SUFFIX}.json
 
-gcc -Wa,--gsframe test.c -o test-fp -fomit-frame-pointer
+PATH=$CUSTOM_BINUTILS_PATH:$PATH gcc -Wa,--gsframe test.c -o test-fp -fomit-frame-pointer
 objdump --sframe test-fp
 cargo run --example dump_sframe test-fp
 cargo run --example create_testcase test-fp
