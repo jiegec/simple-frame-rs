4573508c5b266aa1309e9572bea7867b210bf7d6
Jiajie Chen <c@jia.je>
2026-02-14 17:29:14 +0800

    Add more complex testcase

--- lib/create_testcase.sh
+++ lib/create_testcase.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 set -x -e
-echo "int main() { return 0; }" > test.c
+echo "void foo() {}\nvoid bar() {foo();}\nint main() { return 0; }" > test.c
+
 gcc -Wa,--gsframe test.c -o test
+objdump --sframe test
 cargo run --example dump_sframe test
 cargo run --example create_testcase test
-rm -rf test.c test
+mv testcases/test.json testcases/test-$(uname -m).json
+
+gcc -Wa,--gsframe test.c -o test-fp -fomit-frame-pointer
+objdump --sframe test-fp
+cargo run --example dump_sframe test-fp
+cargo run --example create_testcase test-fp
+mv testcases/test-fp.json testcases/test-fp-$(uname -m).json
+
+rm -rf test.c test test-fp
