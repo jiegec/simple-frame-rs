1f781537afbdbb0da00243de64e6c47ae3995a89
Jiajie Chen <c@jia.je>
2026-02-14 16:07:31 +0800

    Download sframe specs locally for reference

---
+++ spec/download.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl https://sourceware.org/binutils/docs-2.40/sframe-spec.html > sframe-v1-spec.html
+curl https://sourceware.org/binutils/docs-2.45/sframe-spec.html > sframe-v2-spec.html
+curl https://sourceware.org/binutils/docs-2.46/sframe-spec.html > sframe-v3-spec.html
